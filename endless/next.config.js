d94de8e1ea46a3c9b644fae24782d369e0847934
sltrue17-art <sltrue17@gmail.com>
2026-01-17 13:34:39 -0500

    Revert "Add Endless - Advanced Storytelling AI powered by Claude Opus 4.5"

--- endless/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  serverExternalPackages: ['better-sqlite3'],
-}
-
-module.exports = nextConfig
--- endless/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Sidebar } from './Sidebar';
-export { default as ChatArea } from './ChatArea';
-export { default as MessageDisplay } from './MessageDisplay';
-export { default as MessageInput } from './MessageInput';
-export { default as StoryBibleEditor } from './StoryBibleEditor';
-export { default as StoryBiblesList } from './StoryBiblesList';
